55c8b55c06286dc875d2f8fffdd6b0f426ce8f15
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-01-31 22:51:50 +0200

    register admin get request to swagger doc

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -8,6 +8,20 @@ export class AdminRoutes {
   }
 
   private initRoutes(): void {
+    /**
+     * @swagger
+     * /admin:
+     *   get:
+     *     summary: get list of admins added in the application
+     *     tags: [Admin]
+     *     requestBody:
+     *       required: false
+     *     responses:
+     *       200:
+     *         description: admins fetched successfully
+     *       500:
+     *         description: Error fetching admin
+     */
     this.router.get("/admin", async (req: Request, res: Response) => {
       res.status(200).send("admin get endpoint");
     });
