772dcaed2c2e5c6d92fb99b52178198952c40b88
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-01 00:35:07 +0200

    create admin model

---
+++ src/models/Admin.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface AdminModel {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+interface IAdmin extends AdminModel, Document {}
+
+const adminSchema = new Schema<IAdmin>({
+  name: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+export const Admin = model<IAdmin>("Admin", adminSchema);
