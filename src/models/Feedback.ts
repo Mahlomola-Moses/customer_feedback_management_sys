0c6bbe53a1cb633f232c8435bfebcccf476cbeb7
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-02 01:18:18 +0200

    create  customer feedback model

---
+++ src/models/Feedback.ts
@@ -0,0 +1,19 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface FeedbackModel {
+  name: string;
+  email: string;
+  message: string;
+  createdAt: Date;
+}
+
+interface IFeedback extends FeedbackModel, Document {}
+
+const feedbackSchema = new Schema<IFeedback>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Feedback = model<IFeedback>("Feedback", feedbackSchema);
