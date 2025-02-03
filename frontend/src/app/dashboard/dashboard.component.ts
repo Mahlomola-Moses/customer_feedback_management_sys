3f8100ba35115df7612b53f0a9b1359563d8f5ca
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-03 21:30:48 +0200

    create add user modal and integrate api

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -2,23 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../services/admin.service';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   records: any[] = [];
+  showModal = false;
+  newRecord = { name: '', lastname: '', email: '', password: '' };
 
   constructor(
     private adminService: AdminService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.loadRecords();
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    } else {
+      this.loadRecords();
+    }
   }
 
   loadRecords(): void {
@@ -28,6 +37,26 @@ export class DashboardComponent implements OnInit {
       },
       (error) => {
         alert('Failed to load records');
+      }
+    );
+  }
+  openModal(): void {
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+    this.newRecord = { name: '', lastname: '', email: '', password: '' }; // Reset form
+  }
+
+  addRecord(): void {
+    this.adminService.createAdmin(this.newRecord).subscribe(
+      () => {
+        this.loadRecords();
+        this.closeModal();
+      },
+      (error) => {
+        alert('Failed to add record');
       }
     );
   }
