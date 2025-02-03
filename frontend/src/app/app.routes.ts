becc264ca990f3734a69ab7b17c7acccee833be7
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-03 22:17:55 +0200

    create feedback management screen, integrate feedback api pagination

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { FeedbackManagementComponent } from './feedback-management/feedback-manegement.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'feedback-management', component: FeedbackManagementComponent },
   { path: 'feedback', component: FeedbackComponent },
 ];
--- frontend/src/app/services/feedback.service.ts
+++ frontend/src/app/services/feedback.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeedbackService {
-  private apiUrl = 'http://localhost:35050';
+  private apiUrl = 'http://localhost:35050/feedback';
 
   constructor(private http: HttpClient) {}
 
@@ -15,6 +15,14 @@ export class FeedbackService {
     email: string;
     message: string;
   }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/feedback`, feedback);
+    return this.http.post(`${this.apiUrl}`, feedback);
+  }
+
+  getFeedbacks(page: number, limit: number): Observable<any> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString());
+
+    return this.http.get(`${this.apiUrl}`, { params });
   }
 }
