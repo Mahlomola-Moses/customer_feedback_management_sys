1818fc257e8c43d27a44af790379a888ec12ade0
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-03 06:26:56 +0200

    integrate feedback service and update feedback submit form

--- frontend/src/app/services/feedback.service.ts
+++ frontend/src/app/services/feedback.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FeedbackService {
+  private apiUrl = 'http://localhost:35050';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  submitFeedback(feedback: {
+    name: string;
+    email: string;
+    message: string;
+  }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/feedback`, feedback);
+  }
 }
