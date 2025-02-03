184bc3d06075f9803a1f883b611673e8308f76ac
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2025-02-03 04:34:44 +0200

    create login form and login integrate service

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  credentials = { username: '', password: '' };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    this.authService.login(this.credentials).subscribe(
+      (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {
+        alert('Login failed');
+      }
+    );
+  }
 }
